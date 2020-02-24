822d4ae8eabe295bb5bdbda06e7e42d1f925b732
brandon moro <brandon_078@hotmail.com>
2020-02-24 22:58:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
-  def name=(dog_name)
-    @name = dog_name
+  def name=(name)
+    @name = name
   end
   def name
     @dog_name
   end
-  def breed=(dog_breed)
-    @breed = dog_breed
+  def breed=(breed)
+    @breed = breed
   end
   def breed
     @dog_breed
